334e4432e9d45fac628de68e54b86a75c78ecf83
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-11 00:34:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
 const express = require('express');
-const router = express.Router();
-const ContactModel = require('@jworkman-fs/asl').ContactModel;
-const { filterContacts, sortContacts, Pager } = require('@jworkman-fs/asl'); 
+const app = express();
+const port = 3000;
+const contactRouter = require('./contacts');
 
+app.use('/api', contactRouter);
 
-router.get('/contacts', (req, res) => {
-    try {
-        const contacts = ContactModel.getAll();
-
-        // Apply filtering
-        const filtered = filterContacts(contacts, req.get('X-Filter-By'), req.get('X-Filter-Value'), req.get('X-Filter-Operator'));
-
-        // Apply sorting
-        const sorted = sortContacts(filtered, req.query.sort, req.query.direction);
-
-        // Apply pagination
-        const pager = new Pager(sorted, req.query.page, req.query.size);
-        // Set headers
-        res.set("X-Page-Total", pager.total());
-        res.set("X-Page-Next", pager.next());
-        res.set("X-Page-Prev", pager.prev());
-        res.json(pager.results());
-    } catch (e) {
-        switch (e.name) {
-            case "InvalidContactError":
-                return res.status(400).json({ message: e.message });
-            case "ContactNotFoundError":
-                return res.status(404).json({ message: e.message });
-            case "PagerOutOfRangeError":
-                return res.status(400).json({ message: e.message });
-            case "InvalidEnumError":
-                return res.status(400).json({ message: e.message });
-            case "PagerLimitExceededError":
-                return res.status(400).json({ message: e.message });
-            default:
-                return res.status(500).json(e);
-        }
-    }
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
-
-
-module.exports = router;
