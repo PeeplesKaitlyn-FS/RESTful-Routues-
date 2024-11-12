e925b431599c8aeb1dba1bb44b41c695b10d1c4a
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-12 14:47:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 3000;
 const contactRouter = require('./contacts');
 
+router.get('/contacts', (req, res) => {
+    // Handle GET requests to /api/contacts
+    res.json({ message: 'Contacts endpoint' });
+});
+
+module.exports = router;
+
 app.use('/api', contactRouter);
 
 app.get('/', (req, res) => {
