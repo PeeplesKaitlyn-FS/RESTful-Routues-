9ab4c68714235c4fcfab630ba8f17eb3749ad7b5
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-24 18:50:18 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,4 +1,3 @@
-// errors.js
 class ContactResourceError extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -7,6 +6,64 @@ class ContactResourceError extends Error {
   }
 }
 
+
+class ContactNotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ContactNotFoundError';
+  }
+}
+
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+  }
+}
+
+class PagerError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'PagerError';
+  }
+}
+
+class PagerNoResultsError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'PagerNoResultsError';
+  }
+}
+
+class PagerOutOfRangeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'PagerOutOfRangeError';
+  }
+}
+
+class NoContactsFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'NoContactsFoundError';
+    }
+}
+
+class InvalidContactSchemaError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'InvalidContactSchemaError';
+    }
+}
+
 module.exports = {
-  ContactResourceError
+  ContactResourceError,
+  ContactNotFoundError,
+  ApiError,
+  PagerError,
+  PagerNoResultsError,
+  PagerOutOfRangeError,
+  NoContactsFoundError,
+  InvalidContactSchemaError,
 };
