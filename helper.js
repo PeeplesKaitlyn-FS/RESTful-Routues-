479bd3c0711f866f2b93dba39ee7d828792cfd0d
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2024-11-24 19:18:47 -0500

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,25 +1,31 @@
 function filterContacts(contacts, filterBy, filterOperator, filterValue) {
+  const validFields = ['id', 'fname', 'lname', 'phone', 'birthday', 'email'];
+
+
   console.log(`filterBy: ${filterBy}, filterValue: ${filterValue}`);
   console.log(`validFields: ${validFields}`);
+
 
   if (!contacts || contacts.length === 0) {
     throw new NoContactsFoundError('No contacts to filter');
   }
 
+
   if (!filterBy || !filterOperator || !filterValue) {
     throw new InvalidContactSchemaError('Missing filter criteria');
   }
+
 
   if (typeof filterBy !== 'string') {
     throw new InvalidContactSchemaError('filterBy must be a string');
   }
 
-  const validFields = ['id', 'fname', 'lname', 'phone', 'birthday', 'email'];
 
   if (!validFields.includes(filterBy)) {
     console.log(`filterBy: ${filterBy}, validFields: ${validFields}`);
     throw new InvalidContactSchemaError(`Invalid filter field: ${filterBy}`);
   }
+
 
   try {
     const filteredContacts = contacts.filter((contact) => {
@@ -38,12 +44,19 @@ function filterContacts(contacts, filterBy, filterOperator, filterValue) {
       }
     });
 
+
     if (filteredContacts.length === 0) {
       throw new NoContactsFoundError(`No contacts match the filter criteria`);
     }
+
 
     return filteredContacts;
   } catch (error) {
     throw error;
   }
 }
+module.exports = {
+  Pager,
+  filterContacts,
+  sortContacts
+};
